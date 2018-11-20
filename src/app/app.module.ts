9580d08ab4934a0859a7834362561b0d9dc3f664
Shaunak Sheth <shaunak.sheth@gmail.com>
2018-11-20 13:00:37 +0530

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +7,17 @@ import { EmployeeComponent } from './employee/employee.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { EmpPipe } from './pipes/emp.pipe';
 import { EmployeeInfoComponent } from './employee-info/employee-info.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'employeeslist', component: EmployeeInfoComponent},
+  {path: 'employees', component: EmployeeComponent},
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  {path:'**', component: PageNotFoundComponent}
+];
+
 
 @NgModule({
   declarations: [
@@ -16,11 +26,13 @@ import { EmployeeInfoComponent } from './employee-info/employee-info.component';
     EmployeeComponent,
     ContactusComponent,
     EmpPipe,
-    EmployeeInfoComponent
+    EmployeeInfoComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
