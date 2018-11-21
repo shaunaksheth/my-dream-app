4584e9f73848a89448a07312aab8f3cd840a7f41
Shaunak Sheth <shaunak.sheth@gmail.com>
2018-11-21 17:49:01 +0530

    Angular

--- src/app/httpservicecall/httpservicecall.component.ts
+++ src/app/httpservicecall/httpservicecall.component.ts
@@ -10,22 +10,43 @@ import { NgForm } from '@angular/forms';
 })
 export class HttpservicecallComponent implements OnInit {
   items: Items[];
-  constructor(private _shoppingItems:DataService) { }
+  constructor(private _shoppingItems: DataService) { }
 
   ngOnInit() {
     this.getItems();
     console.log(this.items);
-   // this.addItems(frm);  
+    // this.addItems(frm);  
   }
 
-  getItems(){
-    this._shoppingItems.getshoppingitems().subscribe(items=>{this.items=<Items[]>items;console.log(this.items);});
+  getItems() {
+    this._shoppingItems.getshoppingitems().subscribe(items => { this.items = <Items[]>items; console.log(this.items); });
   }
-  addItems(frm:NgForm): void{
+  addItems(frm: NgForm): void {
     console.log(frm.value)
-    let newitem:Items={
-      itemname:frm.value.itemname,
-      itemquantity:frm.value.itemquantity
+    let newitem: Items = {
+      itemname: frm.value.itemname,
+      itemquantity: frm.value.itemquantity
     }
-    this._shoppingItems.addshoppingitems(newitem).subscribe(items=>{this.getItems();})}
+    this._shoppingItems.addshoppingitems(newitem).subscribe(items => { this.getItems(); })
+  }
+  
+  editItems(frm: NgForm): void {
+    console.log(frm.value)
+    let newitem: Items = {
+      id:this.items._id,
+      itemname: frm.value.itemname,
+      itemquantity: frm.value.itemquantity
     }
+    this._shoppingItems.addshoppingitems(newitem).subscribe(items => { this.getItems(); })
+  }
+  
+  deleteItems(frm: NgForm): void {
+    console.log(frm.value)
+    let newitem: Items = {
+      itemname: frm.value.itemname,
+      itemquantity: frm.value.itemquantity
+    }
+    this._shoppingItems.addshoppingitems(newitem).subscribe(items => { this.getItems(); })
+  }
+  
+}
